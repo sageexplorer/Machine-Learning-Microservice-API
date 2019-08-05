48c30879872531361b28cd6bd2ff4bdf3c5b4ffe
sageexplorer <srimal@campusexplorer.com>
2019-08-05 16:42:46 -0700

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sage007/housing
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run housing --image=$dockerpath 
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+name=`kubectl get pods --no-headers -o custom-columns=":metadata.name"`
 
 # Step 4:
+# Get the name of the pod, and forward the port
 # Forward the container port to a host
-
+kubectl port-forward $name 8888:80
